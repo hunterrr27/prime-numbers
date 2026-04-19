c5e0e50fd58b3e57e433daec02d363509eeaf411
Hunter Freeman <132510702+hunterrr27@users.noreply.github.com>
2026-04-19 10:32:44 -0400

    mouse tracking and points

--- makefile
+++ makefile
@@ -1,1 +1,25 @@
-// no clue how to make a makefile tbh 
+
+# Variables
+CC = clang
+CFLAGS = -Wall -std=c11 -I. -I/opt/homebrew/include
+LDFLAGS = -L/opt/homebrew/lib -lraylib -framework IOKit -framework Cocoa -framework OpenGL
+
+# Target executable name
+TARGET = primes
+
+# Source files
+SRC = primes.c
+
+# Default rule
+all: $(TARGET)
+
+$(TARGET): $(SRC)
+	$(CC) $(CFLAGS) $(SRC) -o $(TARGET) $(LDFLAGS)
+
+# Clean up build files
+clean:
+	rm -f $(TARGET)
+
+# Run the program
+run: all
+	./$(TARGET)
--- primes.c
+++ primes.c
@@ -34,8 +34,7 @@ void drawPrimeGraph(int *primes, int countPrimes, int N)
     InitWindow(screenWidth, screenHeight, "Prime Number Explorer");
     SetTargetFPS(60);
 
-    int plotTypeActive = LINEAR; 
-    bool plotTypeEditMode = false;
+    Texture2D title_texture = LoadTexture("assets/title.png");    
 
     // this is mathematical origin since top left 0,0 regarded
     int originX = 50;
@@ -47,91 +46,50 @@ void drawPrimeGraph(int *primes, int countPrimes, int N)
         float xScale = (float)(screenWidth - 100) / countPrimes;
         float yScale = (float)(screenHeight - 100) / countPrimes;
 
+        // get mouse position
+        Vector2 mouse = GetMousePosition();
+
         // linear plot 
         BeginDrawing();
+            // set bg color
             ClearBackground(RAYWHITE);
+
+            //draw imported picture title.png
+            DrawTexture(title_texture, 200, 0, RAYWHITE);
+
+            // draw x and y axes
             DrawLine(originX, originY, screenWidth - 50, originY, BLACK);
             DrawLine(originX, originY, originX, 50, BLACK);
+
+            //draw mouse pointer
+            DrawCircleV(mouse, 3, RED);
 
             // input textbox and dropdown for plotting function
-            GuiLabel((Rectangle){ 12, 62, 140, 24 }, TextFormat("Enter max value: "));
-            GuiLabel((Rectangle){ 12, 10, 140, 24 }, "Plot type:");
-            GuiDropdownBox((Rectangle){ 12, 8 + 24, 140, 28 }, "LINEAR;STEP;POLAR;ULAR", &plotTypeActive, plotTypeEditMode);
+            
 
             for (int i = 0; i < countPrimes; i++) {
-                float plotX = originX + (i * xScale);
-                float plotY = originY - (i * yScale);
+                float plotX = originX + (primes[i] * xScale);
+                float plotY = originY - (primes[i] * yScale);
                 DrawCircle(plotX, plotY, 3, RED);
                 // draw small vertical line across x axis where point is at
                 DrawLine(plotX, originY + 10, plotX, originY - 10, BLACK);
+                // x axis labels
+                DrawText(TextFormat("%d", primes[i]), plotX + 5, originY + 5, 10, BLACK);
                 // draw small horizontal line on y axis where point is at
                 DrawLine(originX + 10, plotY, originX - 10, plotY, BLACK);
+                // y axis labels
+                DrawText(TextFormat("%d", primes[i]), originX - 10, plotY + 5, 10, BLACK);
+
+                // check for mouse collisions
+                // highlight point (x, y) when hovering 
+                if (CheckCollisionPointCircle(mouse, (Vector2){plotX, plotY}, 20)) {
+                    DrawText(TextFormat("(%d, %d)", primes[i], primes[i]), mouse.x + 10, mouse.y - 20, 20, RED);
+                }
             }
+
         EndDrawing();
-
-        // number theory w step function
-        // Inside the while (!WindowShouldClose()) loop
-        /* BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawLine(originX, originY, screenWidth - 50, originY, BLACK);
-            DrawLine(originX, originY, originX, 50, BLACK);
-            for (int i = 0; i < countPrimes - 1; i++) {
-                // Current Prime (Step Start)
-                float x1 = originX + (primes[i] * xScale);
-                float y1 = originY - (i * yScale);
-
-                // Next Prime (Step End)
-                float x2 = originX + (primes[i+1] * xScale);
-                float y2 = originY - ((i + 1) * yScale);
-
-                // 1. Draw horizontal line to the next prime's X position
-                DrawLine(x1, y1, x2, y1, BLUE);
-
-                // 2. Draw vertical line "step up" at the next prime
-                DrawLine(x2, y1, x2, y2, RED);
-                
-                // Optional: Draw a small dot at the jump point
-                DrawCircle(x2, y2, 2, DARKGRAY);
-            }
-        EndDrawing(); */
-
-        // ulam spiral
-        /* BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawLine(originX, originY, screenWidth - 50, originY, BLACK);
-            DrawLine(originX, originY, originX, 50, BLACK);
-
-            for (int i = 0; i < countPrimes; i++) {
-                float plotX = originX + (i * xScale);
-                float plotY = originY - (i * yScale);
-                DrawCircle(plotX, plotY, 3, RED);
-                // draw small vertical line across x axis where point is at
-                DrawLine(plotX, originY + 10, plotX, originY - 10, BLACK);
-                // draw small horizontal line on y axis where point is at
-                DrawLine(originX + 10, plotY, originX - 10, plotY, BLACK);
-            }
-        EndDrawing(); */
-
-
-        // polar plot
-        /* BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawLine(originX, originY, screenWidth - 50, originY, BLACK);
-            DrawLine(originX, originY, originX, 50, BLACK);
-
-            for (int i = 0; i < countPrimes; i++) {
-                float plotX = originX + (i * xScale);
-                float plotY = originY - (i * yScale);
-                DrawCircle(plotX, plotY, 3, RED);
-                // draw small vertical line across x axis where point is at
-                DrawLine(plotX, originY + 10, plotX, originY - 10, BLACK);
-                // draw small horizontal line on y axis where point is at
-                DrawLine(originX + 10, plotY, originX - 10, plotY, BLACK);
-            }
-        EndDrawing(); */
-
-
     }
+    UnloadTexture(title_texture);
     CloseWindow();
 }
 
