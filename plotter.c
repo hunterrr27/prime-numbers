0357faf2f94f22eb8d24ec5df177657314f5b07d
Hunter Freeman <132510702+hunterrr27@users.noreply.github.com>
2026-04-20 10:55:39 -0400

    Delete plotter.c

--- plotter.c
+++
@@ -1,8 +0,0 @@
-// separate file to hold 4 separate plotter algorithms
-
-#include "primes.h"
-
-void linear(int *primes, int countPrimes, int N);
-void step(int *primes, int countPrimes, int N);
-void polar(int *primes, int countPrimes, int N);
-void ulam(int *primes, int countPrimes, int N);
