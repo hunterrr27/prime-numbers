cf9254f67fe96043bfa6f988f26ebdba5ee12e3f
Hunter Freeman <132510702+hunterrr27@users.noreply.github.com>
2026-04-20 11:28:31 -0400

    Rename plot_linear to compute_points_linear

--- src/modes.c
+++ src/modes.c
@@ -1,6 +1,10 @@
 #include "modes.h"
 
-void plot_linear(Appstate *state)
+void compute_points_linear(Appstate *state)
 {
 
 }
+
+void compute_points_step(Appstate *state);
+void compute_points_ulam(Appstate *state);
+void compute_points_polar(Appstate *state);
