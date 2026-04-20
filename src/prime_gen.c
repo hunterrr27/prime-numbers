720fb474d7875879faeea54066221c9d66eebf64
Hunter Freeman <132510702+hunterrr27@users.noreply.github.com>
2026-04-20 11:27:04 -0400

    Update prime_gen.c

--- src/prime_gen.c
+++ src/prime_gen.c
@@ -1,19 +1,19 @@
 #include "prime_gen.c"
 
-bool isPrime(Appstate *state)
+bool isPrime(int n)
 {
-    if (number < 2) return false;
+    if (n < 2) return false;
 
-    int limit = sqrt(number);
+    int limit = sqrt(n);
     for (int i = 2; i <= limit; i++) {
-        if (number % i == 0) {
+        if (n % i == 0) {
             return false;
         }
     }
     return true;
 }
 
-int *generate_primes(Appstate *state)
+void regenerate_primes(Appstate *state)
 {
 
 }
