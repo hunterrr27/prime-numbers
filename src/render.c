2f2d43c1cb611863542f9823096ac7a6bf0f1ff1
Hunter Freeman <132510702+hunterrr27@users.noreply.github.com>
2026-04-20 11:43:52 -0400

    Enhance comments in render.c for clarity
    Add comments to clarify the purpose and responsibilities of the render.c file.

--- src/render.c
+++ src/render.c
@@ -1,3 +1,8 @@
+// purpose is to draw things on the screen using raylib
+// draws the cartesian grid, draws the points, draws lines/shapes/colors, applies zoom and pan, draws other UI elements as needed
+// this file should NOT have UI logic, mode logic, or input handling
+// it takes app data from AppState state and performs raylib Draw() functions
+
 #include "render.h"
 
 void render_points(Appstate *state)
