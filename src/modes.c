b1b7ea4c6567a1e6cc33a4db45afc0f276055509
Hunter Freeman <132510702+hunterrr27@users.noreply.github.com>
2026-04-20 14:11:56 -0400

    Fix Appstate to AppState naming in modes.c

--- src/modes.c
+++ src/modes.c
@@ -1,10 +1,16 @@
 #include "modes.h"
 
-void compute_points_linear(Appstate *state)
+void compute_points_linear(AppState *state)
 {
+  state->pointCount = state->primeCount;
+  state->points = realloc(state->points, sizeof(Point) * state->pointCount);
 
+  for (int i = 0; i < state->primeCount; i++) {
+    state->points[i].x = (float)i;
+    state->points[i].y = (float)state->primes[i];
+  }
 }
 
-void compute_points_step(Appstate *state);
-void compute_points_ulam(Appstate *state);
-void compute_points_polar(Appstate *state);
+void compute_points_step(AppState *state);
+void compute_points_ulam(AppState *state);
+void compute_points_polar(AppState *state);
