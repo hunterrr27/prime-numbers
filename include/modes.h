2858629b2fd1c910b9089d15fa8f833f2508f1d6
Hunter Freeman <132510702+hunterrr27@users.noreply.github.com>
2026-04-20 11:22:14 -0400

    Update modes.h

--- include/modes.h
+++ include/modes.h
@@ -1,7 +1,13 @@
-typedef enum {
-  LINEAR,
-  STEP,
-  POLAR,
-  ULAM,
-  COUNT
-} PlotMode;
+#ifndef MODES_H
+#define MODES_H
+
+#include "state.h"
+
+void compute_points_linear(AppState *state);
+void compute_points_step(AppState *state);
+void compute_points_ulam(AppState *state);
+void compute_points_polar(AppState *state);
+
+void regenerate_points(AppState *state);
+
+#endif
