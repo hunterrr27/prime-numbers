3cba96f33da3befea36e0d929f0f4f7f8517c446
Hunter Freeman <132510702+hunterrr27@users.noreply.github.com>
2026-04-20 11:45:53 -0400

    Update ui.c

--- src/ui.c
+++ src/ui.c
@@ -1,3 +1,7 @@
+// purpose is to handle user interaction and update the AppState
+// detects kbm i/o, handles raygui widgets, opens text input boxes, switches visualization modes, sets needsRecompute = true when needed, adjusts zoom and pan, etc
+// ui.c does NOT draw points, do math, or generate prime numbers
+
 #include "ui.h"
 
 void handle_input(Appstate *state)
