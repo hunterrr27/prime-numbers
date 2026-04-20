b7d5f4ff310c738dbb6b4e7b59796e84af42ffd4
Hunter Freeman <132510702+hunterrr27@users.noreply.github.com>
2026-04-20 11:21:50 -0400

    Add render functions to render.h header file

--- include/render.h
+++ include/render.h
@@ -1,1 +1,11 @@
+#ifndef RENDER_H
+#define RENDER_H
+
+#include "state.h"
+
+void render_grid(const AppState *state);
+void render_points(const AppState *state);
+void render_ui(const AppState *state);
+
+#endif
 
