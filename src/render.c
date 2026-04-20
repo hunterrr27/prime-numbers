a988974592b3e9cf7843180c34a2d8e5b1cf0017
Hunter Freeman <132510702+hunterrr27@users.noreply.github.com>
2026-04-20 14:39:04 -0400

    Comment out UI rendering code in render_ui function

--- src/render.c
+++ src/render.c
@@ -15,12 +15,27 @@ void render_points(const AppState *state)
 
 void render_grid(const AppState *state)
 {
-
+    // draw x and y axis
+    // x axis DrawLine(50, GetScreenHeight() - 50, GetScreenWidth() - 50, GetScreenHeight - 50);
+    // y axis DrawLine(50, 50, 50, GetScreenHeight() - 50);
 
 }
 
 void render_ui(const AppState *state)
 {
+    // draw N text input box
+    // Rectangle box = {GetScreenWidth() / 2 - 200, GetScreenHeight() / 2 - 60, 400, 120};
+    // GUITextInputBox(Rectangle){box, "Enter N", "Type a new value for N", state.inputbuffer, sizeof(state.inputbuffer), true};
+    
+    // draw plot mode dropdown box
+    // GUIDropdownBox(Rectangle){box, "Choose a plot mode", MODE_LINEAR;MODE_STEP;MODE_POLAR;MODE_ULAM;MODE_COUNT, state.inputbuffer, sizeof(state.inputbuffer), true};
+    
+    // draw mouse (x,y) coordinates in bottom left of screen 
+    // Vector 2 mouse = GetMousePosition();
+    // DrawText(FormatText("(%d,%d)", mouse.x, mouse.y), 50, GetScreenHeight() - 50, 10, RED); 
+
+    // draw FPS counter in bottom left near mouse coordinates
+    // DrawFPS();
 
 
 }
